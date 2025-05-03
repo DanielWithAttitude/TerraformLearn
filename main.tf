8aee8f129496836991bbd4e926c150da7983cb70
Daniel <daniel.wysiecki@PROTONMAIL.COM>
2025-05-03 19:38:08 +0200

    Added user-data and sg declaration

--- main.tf
+++ main.tf
@@ -10,3 +10,20 @@ terraform {
 provider "aws" {
   region = "eu-north-1"
 }
+
+resource "aws_instance" "webserver" {
+  count = 2
+  ami = var.instance_ami
+  instance_type = var.instance_ec2_type
+  security_groups = [ aws_security_group.webserver_instances_sg ]
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World!" > index.html
+            python3 -m http.server 8080 &
+            EOF
+}
+
+resource "aws_security_group" "webserver_instances_sg" {
+  name = "webserver_instances_sg"
+}
+

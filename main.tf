9910f5c8097d37bb6cbbdb3e2af9f3d67a6ab6fb
Daniel <daniel.wysiecki@PROTONMAIL.COM>
2025-05-03 19:52:00 +0200

    Added Security Groups and changed user data

--- main.tf
+++ main.tf
@@ -12,18 +12,39 @@ provider "aws" {
 }
 
 resource "aws_instance" "webserver" {
-  count = 2
-  ami = var.instance_ami
-  instance_type = var.instance_ec2_type
-  security_groups = [ aws_security_group.webserver_instances_sg ]
-  user_data = <<-EOF
-            #!/bin/bash
-            echo "Hello, World!" > index.html
-            python3 -m http.server 8080 &
-            EOF
+  count             = 2
+  ami               = var.instance_ami
+  instance_type     = var.instance_ec2_type
+  security_groups   = [ aws_security_group.webserver_instances_sg ]
+  user_data         = base64encode(<<-EOF
+                    #!/bin/bash
+                    yum update -y
+                    yum install -y httpd
+                    systemctl start httpd
+                    systemctl enable httpd
+                    echo "Hello from $(hostname)" > /var/www/html/index.html
+                    EOF
+  )
 }
 
 resource "aws_security_group" "webserver_instances_sg" {
   name = "webserver_instances_sg"
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [ load_balancer_web_security_group ]
+  }
 }
+
+resource "aws_security_group" "load_balancer_sg" {
+  name = "load_balancer_web_security_group"
+  ingress = {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
 
